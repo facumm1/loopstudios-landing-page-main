e59fd7a6e360dc9e962f4abd4d4af2d38c0f9dcd
Facu <facundomamani120@gmail.com>
2022-05-20 23:02:03 -0300

    Main responsive realizado, comenzando footer responsive

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const navToggler = document.querySelector('.nav-toggler');
 const headerNavbar = document.querySelector('.navbar-nav');
 const closeBtn = document.querySelector('.close-btn');
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
 
 navToggler.addEventListener('click', () => {
     headerNavbar.style.left = 0;
+    main.style.display = 'none';
+    footer.style.display = 'none';
 });
 
 closeBtn.addEventListener('click', () => {
     headerNavbar.style.left = -150 + '%';
+    main.style.display = 'block';
+    /* footer.style.display = 'block'; */
 });
